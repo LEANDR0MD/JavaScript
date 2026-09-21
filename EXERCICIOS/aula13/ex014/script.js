5d5a9d5b80844ed407ac2bb12b1f273e66d3761b
Leandro De Melo Dias <167830186+LEANDR0MD@users.noreply.github.com>
2026-09-21 20:41:45 -0300

    aulas

--- EXERCICIOS/aula13/ex014/script.js
+++ EXERCICIOS/aula13/ex014/script.js
@@ -1,1 +1,57 @@
-alert(`Olá...`)
+function verificar() {
+
+    var data = new Date()
+    var ano = data.getFullYear()
+    var nascimento = window.document.getElementById('ididade')
+    var res = window.document.getElementById('res')
+
+    if (nascimento.value.length == 0 || nascimento.value > ano) {
+        window.alert('[ERRO] informe uma data valida!')
+    } else {
+
+        var sexo = window.document.getElementsByName('nsexo')
+        var idade = ano - Number(nascimento.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (sexo[0].checked) {
+            genero = 'Homem'
+
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'imagens/hcriaca.png')
+            } else if (idade < 20) {
+                img.setAttribute('src', 'imagens/menino.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'imagens/homem.png')
+            } else {
+                img.setAttribute('src', 'imagens/idoso.png')
+            }
+
+        } else if (sexo[1].checked) {
+            genero = 'Mulher'
+
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src', 'imagens/mcrianca.png')
+            } else if (idade < 20) {
+                img.setAttribute('src', 'imagens/menina.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'imagens/mulher.png')
+            } else {
+                img.setAttribute('src', 'imagens/idosa.png')
+            }
+
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Resultado ${genero} com ${idade} anos`
+        res.appendChild(img)
+
+
+
+
+    }
+
+
+
+
+}
