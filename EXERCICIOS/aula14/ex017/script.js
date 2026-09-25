65ea2a6c4503fff436bcadc719eb5826903b2c85
Leandro De Melo Dias <167830186+LEANDR0MD@users.noreply.github.com>
2026-09-24 22:28:37 -0300

    aulas

---
+++ EXERCICIOS/aula14/ex017/script.js
@@ -0,0 +1,15 @@
+function contador() {
+
+    var inicio = window.document.getElementById('idinicio')
+    var res = window.document.getElementById('res')
+
+    var ini = Number(inicio.value)
+
+    res.innerHTML = `teste ${ini}`
+
+
+
+
+
+
+}
